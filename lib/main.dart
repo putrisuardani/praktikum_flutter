5845ef0872ad61587dbd8eba96e7b438473421a4
putrisuardani <putrisuardani94@gmail.com>
2026-04-07 12:47:55 +0800

    Praktikum 4 - Revisi home pada main dart

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: DetailProfile(nama: "Luh Gede Putri Suardani"),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
