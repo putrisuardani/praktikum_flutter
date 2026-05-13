915cef418bb0848e044dff3b5abab78fec56b334
putrisuardani <putrisuardani94@gmail.com>
2026-05-13 20:22:26 +0800

    add login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:praktikum_flutter/provider/profile_provider.dart';
-import 'package:praktikum_flutter/screens/list_profile.dart';
+import 'package:praktikum_flutter/screens/login.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -35,7 +36,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: ListProfile(),
+      home: Login(),
     );
   }
 }
